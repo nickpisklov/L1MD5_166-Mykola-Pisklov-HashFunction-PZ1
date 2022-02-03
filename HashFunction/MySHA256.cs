01b88abdef1b4b3e1b1f42f278b9a1bf7aa52c9c
nickpisklov <91736375+nickpisklov@users.noreply.github.com>
2022-02-03 11:52:52 +0200

    Add files via upload

---
+++ HashFunction/MySHA256.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace HashFunction
+{
+    public static class MySHA256
+    {
+        public static string GetHashCodeForMessage(string message)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(message));
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    stringBuilder.Append(bytes[i].ToString("x2"));
+                }
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
---
+++ HashFunction/Program.cs
@@ -0,0 +1,6 @@
+﻿using HashFunction;
+
+string message = "Hello";
+string hash = MySHA256.GetHashCodeForMessage(message);
+Console.WriteLine("Hash data for message '{0}' is {1}", message, hash);
+    
